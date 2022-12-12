c3ac94b849f6c01966243a0443a30ed535efa022
Oleh Sytnik <osyt@telenor.dk>
2022-12-12 21:08:45 +0200

    lesson 28 code added

---
+++ Lesson28/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+var windowsAuth = "Integrated Security=True;";
+var sqlAuth = "User=sa;Password=sa/ics5603;";
+var connectionString = $"Server=127.0.0.1;{sqlAuth}Database=newdb;";
+var users = new List<User1>();
+
+ReadUsersSqlClient(users, connectionString);
+var users2 = users;
+
+static void ReadUsersSqlClient(ICollection<User1> users, string conn)
+{
+    using var connection = new SqlConnection(conn);
+    var command = new SqlCommand("select * from users", connection);
+    connection.Open();
+    var reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        ParseSingleRow(users, reader);
+    }
+
+    reader.Close();
+}
+
+static void OutputSingleRow(IDataRecord dataRecord) =>
+    Console.WriteLine($"{dataRecord[0]}, {dataRecord[1]}, " +
+                      $"{dataRecord[2]}, {dataRecord[3]}");
+
+static void ParseSingleRow(ICollection<User1> users, IDataRecord dataRecord)
+{
+    var user = new User1(dataRecord.GetInt32(0),
+        dataRecord.GetString(1),
+        dataRecord.IsDBNull(2) ? "-" : dataRecord.GetString(2),
+        dataRecord.IsDBNull(3) ? "-" : dataRecord.GetString(3));
+    users.Add(user);
+    Console.WriteLine($"User: Id: {user.Id1}, FirstName: {user.FirstName1}, " +
+                      $"LastName: {user.LastName1}, Info: {user.Info1}");
+}
+
+public class User1
+{
+    public int Id1 { get; set; }
+    public string FirstName1 { get; set; }
+    public string LastName1 { get; set; }
+    public string Info1 { get; set; }
+
+    public User1(int id, string firstName1, string lastName1, string info1)
+    {
+        Id1 = id;
+        FirstName1 = firstName1;
+        LastName1 = lastName1;
+        Info1 = info1;
+    }
+}
---
+++ Template/Program.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Searchelement(string searche, List<string> mainlist)
+        {
+            int number=-1;
+
+            for (int i = 0; i< mainlist.Count; i++)
+
+            {
+                if (searche == mainlist[i])
+
+                {
+                    number = i; break;
+                }
+            }
+
+            return number;
+        }
+        
+
+
+        static void Main()
+        {
+            List<string> FirstName = new List<string>() { "Tom", "Bob", "Sam" }, LastName = new List<string>() { "Tom", "Bob", "Sam" }, Number = new List<string>() { "+79", "+76", "+78" };
+
+            int result = -5;
+            int option = -5;
+
+
+            while (option != -1)
+            {
+                Console.WriteLine("1.Search by last name \n" +
+                              "2.Search by name \n" +
+                              "3.Search by number \n" +
+                              "4.Add new contact \n" +
+                              "5.All contact \n" +
+                              "-1.End");
+                option = Convert.ToInt32(Console.ReadLine());
+                string element;
+                switch (option)
+                {
+                    case 1:
+                        element = Console.ReadLine();
+                        result = Searchelement(element, FirstName);
+                        Console.WriteLine("Number: " + result + " Name:" + FirstName[result] + " LastName: " + LastName[result] + " Number:" + Number[result] + "\n");
+                        break;
+                    case 2:
+                        element = Console.ReadLine();
+                        result = Searchelement(element, LastName);
+                        Console.WriteLine("Number: " + result + " Name:" + FirstName[result] + " LastName: " + LastName[result] + " Number:" + Number[result] + "\n");
+                        break;
+                    case 3:
+                        element = Console.ReadLine();
+                        result = Searchelement(element, Number);
+                        Console.WriteLine("Number: "+result+" Name:" + FirstName[result] + " LastName: " + LastName[result] + " Number:" + Number[result] + "\n");
+                        break;
+                    case 4:
+                        FirstName.Add(Console.ReadLine());
+                        LastName.Add(Console.ReadLine());
+                        Number.Add(Console.ReadLine());
+                        break;
+                    case 5:
+                        for (int i=0;i<FirstName.Count;i++)
+                        {
+                            Console.WriteLine("Number:"+i+" Name:"+ FirstName[i] + " LastName: " + LastName[i]+ " Number:" + Number[i]+"\n");
+                        }
+                        break;
+                    case -1:
+                        result = -1;
+                        break;
+                }
+            }
+
+        }
+    }
+}
