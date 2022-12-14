a5ebeab3ea43e1a449504b1bec86a4b940e514b7
Oleh Sytnik <osyt@telenor.dk>
2022-12-14 21:49:25 +0200

    lesson 29 code updated

--- Lesson28/Program.cs
+++ Lesson28/Program.cs
@@ -1,13 +1,84 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using Bogus;
+using Dapper;
 
 var windowsAuth = "Integrated Security=True;";
 var sqlAuth = "User=sa;Password=sa/ics5603;";
 var connectionString = $"Server=127.0.0.1;{sqlAuth}Database=newdb;";
 var users = new List<User1>();
-
-ReadUsersSqlClient(users, connectionString);
+await GetJoinedData(connectionString);
+// await DeleteSomeDataById(connectionString);
+// await InsertViaDapper(connectionString);
+// ReadUsersSqlClient(users, connectionString);
+// await UseDapper(connectionString);
 var users2 = users;
+
+static async Task UseDapper(string conn)
+{
+    var someId = 3;
+    await using var connection = new SqlConnection(conn);
+    var users = (await connection
+        .QueryAsync<User>(
+            "select * from users where id=@userId",
+            new {userId = someId})).ToList();
+    var infos = (await connection
+        .QueryAsync<DetailsInfo1>("select * from detailsinfo")).ToList();
+    Console.WriteLine("");
+}
+
+static async Task InsertViaDapper(string conn)
+{
+    // generate some data
+    var seed = new Faker<User>()
+        .RuleFor(user => user.Id,
+            faker => faker.IndexFaker + 1)
+        .RuleFor(person => person.FirstName,
+            faker => faker.Name.FirstName())
+        .RuleFor(person => person.LastName,
+            faker => faker.Name.LastName())
+        .RuleFor(person => person.Info,
+            faker => faker.Lorem.Paragraph());
+    var users = seed.Generate(100);
+    await using var connection = new SqlConnection(conn);
+    foreach (var user in users)
+    {
+        connection.Execute("insert into Users values (@Id, @FirstName, @LastName, @Info)",
+            new {user.Id, user.FirstName, user.LastName, user.Info});
+    }
+
+    Console.WriteLine("");
+}
+
+static async Task UpdateWithDapper(string conn)
+{
+    var someInfo = "info to be updated";
+    var someUserId = 10;
+    await using var connection = new SqlConnection(conn);
+    await connection.ExecuteAsync(
+        "update users set info=@someInfo where id=@someUserId",
+        new {someInfo, someUserId});
+}
+
+static async Task DeleteSomeDataById(string conn)
+{
+    var someId = 11;
+    await using var connection = new SqlConnection(conn);
+    await connection.ExecuteAsync(
+        "delete from users where id=@someId",
+        new {someId});
+}
+
+static async Task GetJoinedData(string conn)
+{
+    await using var connection = new SqlConnection(conn);
+    var users = (await connection
+            .QueryAsync<JoinResult>(
+                "select * from DetailsInfo di full outer join Users us on di.UserId = us.Id"))
+        .ToList();
+    Console.WriteLine();
+}
+
 
 static void ReadUsersSqlClient(ICollection<User1> users, string conn)
 {
@@ -37,6 +108,64 @@ static void ParseSingleRow(ICollection<User1> users, IDataRecord dataRecord)
     Console.WriteLine($"User: Id: {user.Id1}, FirstName: {user.FirstName1}, " +
                       $"LastName: {user.LastName1}, Info: {user.Info1}");
 }
+
+public class User
+{
+    public int Id;
+    public string FirstName;
+    public string LastName;
+    public string Info;
+}
+
+// public record User(int Id, string FirstName, string LastName, string Info);
+
+public record JoinResult
+{
+    public JoinResult()
+    {
+    }
+
+    public JoinResult(int InfoId, string Data, int UserId, string FirstName,
+        int Id, string LastName, string Info)
+    {
+        this.InfoId = InfoId;
+        this.Data = Data;
+        this.UserId = UserId;
+        this.FirstName = FirstName;
+        this.Id = Id;
+        this.LastName = LastName;
+        this.Info = Info;
+    }
+
+    public int InfoId { get; init; }
+    public string Data { get; init; }
+    public int UserId { get; init; }
+    public string FirstName { get; init; }
+    public int Id { get; init; }
+    public string LastName { get; init; }
+    public string Info { get; init; }
+
+    public void Deconstruct(out int InfoId, out string Data, out int UserId, out string FirstName, out int Id,
+        out string LastName, out string Info)
+    {
+        InfoId = this.InfoId;
+        Data = this.Data;
+        UserId = this.UserId;
+        FirstName = this.FirstName;
+        Id = this.Id;
+        LastName = this.LastName;
+        Info = this.Info;
+    }
+}
+
+
+public class DetailsInfo1
+{
+    public string Data { get; set; }
+    public int UserId { get; set; }
+    public int Id { get; set; }
+}
+
 
 public class User1
 {
